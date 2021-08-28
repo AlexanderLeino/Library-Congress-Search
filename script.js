ae3bab51c25d1746da881b477e5f0bef4b5c70e9
Alexander Leino <Alexander.Leino@yahoo.com>
2021-08-28 13:55:06 -0400

    class ended

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var submitbtn = document.getElementById('submitbtn')
+var userInputText = document.getElementById('userTextInput').value
 submitbtn.addEventListener('click',filterResults)
 
-var baseUrl = https://www.loc.gov/search/
+var baseUrl = `https://www.loc.gov/? +${maps}&q=Iceland&fo=json`
 
 
 
-function filterResults() {
-    
-    fetch()
-        .then(function (response) {
-          if (response.ok) {
-             .then response.json();
-          }
-    
-          return response.json();
-        })
-    
+function filterResults(baseUrl) {
+    fetch(baseUrl)
+   .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 }
+
+    console.log(userInputText)
+    
+filterResults(baseUrl)
